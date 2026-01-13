8763c6ffdb0974e3b5c19dbf97fd67fbeab0b360
TuanPhong230506 <phongnt230506@gmail.com>
2026-01-13 19:39:26 +0700

    new content

--- 12-01-2026/main.js
+++ 12-01-2026/main.js
@@ -3,3 +3,14 @@ var fullName = 'Nguyen Tuan Phong';
 // alert(fullName);
 // alert(age);
 
+var a = 1;
+var b = 2;
+
+// if (a == b) {
+//     alert('Dung');
+// }
+
+if (a > 0 && b > 0) {
+    alert ('a va b lon hon khong')
+}
+
