5d102cc49363e4147c936b70547493152bfe0682
TuanPhong230506 <phongnt230506@gmail.com>
2026-01-13 19:49:14 +0700

    next lesson

--- 12-01-2026/main.js
+++ 12-01-2026/main.js
@@ -3,14 +3,19 @@ var fullName = 'Nguyen Tuan Phong';
 // alert(fullName);
 // alert(age);
 
-var a = 1;
-var b = 2;
+// var a = 1;
+// var b = 2;
 
 // if (a == b) {
 //     alert('Dung');
 // }
 
-if (a > 0 && b > 0) {
-    alert ('a va b lon hon khong')
-}
+// if (a > 0 && b > 0) {
+//     alert ('a va b lon hon khong')
+// }
 
+var a = 8;
+var b = 2;
+
+var c = a % b;
+alert (c)
